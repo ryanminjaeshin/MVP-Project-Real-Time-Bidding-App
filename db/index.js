29a5ed3397d57fe2adc655818f4e4c5e6f1f1b4b
Ryan Shin <ryanminjaeshin@gmail.com>
2020-11-02 11:57:08 -0800

    Fix typo and export Gallery Model

--- db/index.js
+++ db/index.js
@@ -25,3 +25,7 @@ const gallerySchema = new mongoose.Schema({
 const Gallery = mongoose.model('Gallery', gallerySchema);
 
 /*---------------------------------------------------------*/
+
+module.exports = {
+  Gallery,
+};
