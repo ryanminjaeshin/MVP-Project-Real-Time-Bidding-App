28d86c7eda0c10bd517f78bb63cdda1b024f7948
Ryan Shin <ryanminjaeshin@gmail.com>
2020-11-02 20:52:03 -0800

    Finish basic db set up

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -9,22 +9,59 @@ export default class App extends React.Component {
   constructor() {
     super();
     this.state = {
+      id: 0,
+      name: '',
+      description: '',
+      startingBid: 0,
+      image: '',
     };
+    this.getOneItem = this.getOneItem.bind(this);
   }
 
   componentDidMount() {
+    this.getOneItem(1);
   }
 
+  getOneItem(id) {
+    axios.get(`/gallery/${id}`)
+      .then((res) => {
+        this.setState(() => ({
+          id: res.data.id,
+          name: res.data.name,
+          description: res.data.description,
+          startingBid: res.data.startingBid,
+          image: res.data.image,
+        }), () => {
+          console.log(this.state);
+        });
+      });
+  }
+
+  // eslint-disable-next-line class-methods-use-this
   refreshTimer() {
     window.location.reload(false);
   }
 
   render() {
+    const {
+      id,
+      name,
+      description,
+      startingBid,
+      image,
+    } = this.state;
+
     return (
       <div>
-        <ProductInfo />
+        <ProductInfo
+          id={id}
+          name={name}
+          description={description}
+          startingBid={startingBid}
+          image={image}
+        />
         <Timer refreshTimer={this.refreshTimer} />
-        <Bid refreshTimer={this.refreshTimer}/>
+        <Bid refreshTimer={this.refreshTimer} />
       </div>
     );
   }
